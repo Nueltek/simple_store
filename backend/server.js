b79ac59e51bfd05766d099377add12c73b3fb97f
nueltek <emmathics@gmail.com>
2025-07-27 02:41:44 +0100

    fix bug in server connections

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/products", productRoutes); // Use product routes
 
 // Serve static files from the React frontend app
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
